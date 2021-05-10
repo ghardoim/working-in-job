4270a23595f750faa8966a3f394f302f3e4febb2
Gabriel Hardoim <ghardoim@hotmail.com>
2021-05-10 20:40:57 -0300

    novas correções

--- daniel/exercicio1.py
+++ daniel/exercicio1.py
@@ -1,15 +1,20 @@
 pesos = (3, 5, 2)
 alunos = {}
 
-for i in range(10):
+for i in range(3):
   nome = input(f"Digite o nome do {i + 1}° aluno: ")
 
   notas = []
   for j in range(3):
     notas.append(float(input(f"Informe a {j + 1}° nota: ")))
 
-  total = sum([ notas[n] * pesos[n] for n in range(3) ])
-  alunos[nome] = total / sum(pesos)
+    total = 0
+    soma_pesos = 0
+    for n in range(3):
+      total += notas[n] * pesos[n]
+      soma_pesos += pesos[n]
+
+    alunos[nome] = total / soma_pesos
 
 for i in range(5):
   nome = input("Informe qual aluno deseja visualizar a média: ")
--- daniel/exercicio2.py
+++ daniel/exercicio2.py
@@ -1,4 +1,8 @@
 lista = [4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196]
 nova_lista = [ int(n ** 0.5) for n in lista ]
 
-print(f"A média da raiz quadrada da lista é {sum(nova_lista) / len(nova_lista)}")
+soma = 0
+for n in nova_lista:
+  soma += n
+
+print(f"A média da raiz quadrada da lista é {soma / len(nova_lista)}")
