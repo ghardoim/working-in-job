e12df0a91eab413163efd497aebb5166ea18647a
Gabriel Hardoim <ghardoim@hotmail.com>
2021-05-10 18:18:19 -0300

    outras correções

--- daniel/exercicio2.py
+++ daniel/exercicio2.py
@@ -1,6 +1,4 @@
-from math import sqrt
-
 lista = [4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196]
-nova_lista = [ int(sqrt(n)) for n in lista ]
+nova_lista = [ int(n ** 0.5) for n in lista ]
 
 print(f"A média da raiz quadrada da lista é {sum(nova_lista) / len(nova_lista)}")
--- daniel/exercicio3.py
+++ daniel/exercicio3.py
@@ -2,6 +2,13 @@ lancamentos = (1, 1, 2, 6, 6, 6, 4, 1, 3, 3, 1, 2, 2, 6, 5, 5, 1, 3, 5, 4, 2, 1,
 
 dado = {}
 for i in range(1, 7):
-  dado[str(i)] = f"{int(lancamentos.count(i) / len(lancamentos) * 100)}%"
+
+  qnt_lancamentos = 0
+  for lancamento in lancamentos:
+
+    if lancamento == i:
+      qnt_lancamentos += 1
+
+  dado[str(i)] = f"{int(qnt_lancamentos / len(lancamentos) * 100)}%"
 
 print(dado)
